ca10a71efe2183b7cfd500d3d30475ac491a1984
Djaouti Mohamed <djaouti.mohamed@gmail.com>
2022-03-03 16:17:32 +0100

    Container de service, autowiring, créer son propre service

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController
 {
-    protected $logger;
-    public function __construct(LoggerInterface $logger)
+    protected $calculator;
+    public function __construct(Calculator $calculator)
     {
-        $this->logger = $logger;
+        $this->calculator = $calculator;
     }
+    // // Appeler le service LoggerInterface
+    // protected $logger;
+    // public function __construct(LoggerInterface $logger)
+    // {
+    //     $this->logger = $logger;
+    // }
     public function hello($prenom)
     {
-        $this->logger->error("Messge d'erreure");
+        // Quand tu appel le hello controller tu fait un calcule du Tva avec une valeure donnée.
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
+        // $this->logger->error("Mon mesage de log !");
         // $request = Request::createFromGlobals();
         // $prenom = $request->attributes->get("prenom");
         return new Response("Hello $prenom");
