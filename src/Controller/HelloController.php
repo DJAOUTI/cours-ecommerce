9cba89ab5c64d6ba24624c2d9548764523f0ca25
Djaouti Mohamed <djaouti.mohamed@gmail.com>
2022-03-05 19:35:17 +0100

    Finich my commit

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-<<<<<<< HEAD
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,32 +13,6 @@ class HelloController
      */
     public function hello($prenom)
     {
-=======
-use App\Taxes\Calculator;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-class HelloController
-{
-    protected $calculator;
-    public function __construct(Calculator $calculator)
-    {
-        $this->calculator = $calculator;
-    }
-    // // Appeler le service LoggerInterface
-    // protected $logger;
-    // public function __construct(LoggerInterface $logger)
-    // {
-    //     $this->logger = $logger;
-    // }
-    public function hello($prenom)
-    {
-        // Quand tu appel le hello controller tu fait un calcule du Tva avec une valeure donnée.
-        $tva = $this->calculator->calcul(100);
-        dump($tva);
-        // $this->logger->error("Mon mesage de log !");
->>>>>>> ca10a71efe2183b7cfd500d3d30475ac491a1984
         // $request = Request::createFromGlobals();
         // $prenom = $request->attributes->get("prenom");
         return new Response("Hello $prenom");
