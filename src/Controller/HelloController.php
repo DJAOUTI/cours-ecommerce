68868a11a1e95bbc6efe2ae9be8fc2843bfd011b
Djaouti Mohamed <djaouti.mohamed@gmail.com>
2022-03-12 22:47:29 +0100

    Découverte de la notion de container

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,19 +2,37 @@
 
 namespace App\Controller;
 
+
+use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Log\Logger;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+
+
+    protected $calculator;
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+
     /**
      * @Route("/hello/{prenom?world}", name="hello")
      */
-    public function hello($prenom)
+    public function hello($prenom, Slugify $slugify)
     {
+
+        $slugify = $slugify->Slugify("Hello World");
+        dump($slugify);
+
         $tva = $this->calculator->calcul(100);
         dump($tva);
+        // $this->logger->error("Mon message de Log");
         // $request = Request::createFromGlobals();
         // $prenom = $request->attributes->get("prenom");
         return new Response("Hello $prenom");
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController
 {
+    // Injection de dépendance pour le service Taxes-> Calculator
+    protected $calculator;
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
 
     // Méthode index
     public function index()
     {
-        $tva = $this->calculator->calcul(600000);
+        $tva = $this->calculator->calcul(2500);
         dump($tva);
-
         dd("Ca fonctionne !"); //On peut remplacer var-dump() par juste le mot dump (une fonction prè-définie)
         //die(); On peut remplacer le dump et le die par juste le mot dd (dump and die)
     }
