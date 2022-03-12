303e4b9c2dce1c1b096d72cca4ee59db998fe298
Djaouti Mohamed <djaouti.mohamed@gmail.com>
2022-03-12 12:30:16 +0100

    Reprise de la formation: Les limites du Container : configuration des services

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
     /**
      * @Route("/hello/{prenom?world}", name="hello")
      */
     public function hello($prenom)
     {
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         // $request = Request::createFromGlobals();
         // $prenom = $request->attributes->get("prenom");
         return new Response("Hello $prenom");
--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -2,11 +2,19 @@
 
 namespace App\Taxes;
 
+use Psr\Log\LoggerInterface;
+
 class Calculator
 {
+    protected $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     public function calcul(float $prix): float
     {
+        $this->logger->info("Un calcul de la tva sur le prix : $prix");
         // Tva est à 20% dan cet exemple
         return $prix * (20 / 100);
     }
